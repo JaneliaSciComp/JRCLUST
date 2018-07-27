1ce8e978b0a339d06030ccf0011cee62d82f8df4
Alan Liddell <alan@vidriotech.com>
2018-07-27 11:46:02 -0400

    Tim's changes to mean_excl_

--- mean_excl_.m
+++ mean_excl_.m
@@ -8,6 +8,6 @@ function vnWav1_mean = mean_excl_(mnWav1, P)
         nSites_all = size(mnWav1, 2);
         nSites_excl = numel(viSiteZero);
         nSites = nSites_all - nSites_excl;
-        vnWav1_mean = int16((sum(mnWav1,2) - sum(mnWav1(:,nSites_excl),2)) / nSites);
+        vnWav1_mean = int16((sum(mnWav1,2) - sum(mnWav1(:,viSiteZero),2)) / nSites); % TW BUGFIX
     end
 end %func
