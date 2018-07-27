8048b7603cbff3f75768aa7489931ca9091d483b
Alan Liddell <alan@vidriotech.com>
2018-07-27 10:58:09 -0400

    Tim's changes to button_CluWav_simulate_

--- button_CluWav_simulate_.m
+++ button_CluWav_simulate_.m
@@ -1,5 +1,6 @@
 %--------------------------------------------------------------------------
-function S0 = button_CluWav_simulate_(iCluCopy, iCluPaste, S0)
+function S0 = button_CluWav_simulate_(iCluCopy, iCluPaste, S0, initial_load) % TW
+    if nargin<4,  initial_load=0; end
     if nargin<3,  S0 = get(0, 'UserData'); end
     if nargin<2, iCluPaste = []; end
     if iCluCopy == iCluPaste, iCluPaste = []; end
@@ -8,10 +9,14 @@ function S0 = button_CluWav_simulate_(iCluCopy, iCluPaste, S0)
 
     S0 = update_cursor_(S0, iCluCopy, 0);
     S0 = update_cursor_(S0, iCluPaste, 1);
-    S0 = keyPressFcn_cell_(get_fig_cache_('FigWav'), {'j','t','c','i','v','e','f'}, S0); %'z' to recenter
+    if initial_load==0
+        S0 = keyPressFcn_cell_(get_fig_cache_('FigWav'), {'t','c','i','v','e','f'}, S0); %'z' to recenter 'j', %TW
+    else
+        S0 = keyPressFcn_cell_(get_fig_cache_('FigWav'), {'j','t','c','i','v','e','f'}, S0); %'z' to recenter 'j', %TW
+    end
     set(0, 'UserData', S0);
 
-    auto_scale_proj_time_(S0);
+    % auto_scale_proj_time_(S0); % TW
     plot_raster_(S0); %psth
     figure_wait_(0, hFig);
 end
