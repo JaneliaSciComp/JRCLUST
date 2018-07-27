b1558dc34e4f3fbb550af37e226f952dcdb4c868
Alan Liddell <alan@vidriotech.com>
2018-07-27 11:38:11 -0400

    Tim's changes to manual_

--- manual_.m
+++ manual_.m
@@ -63,7 +63,7 @@ function manual_(P, vcMode)
     % hFigProj, hFigHist, hFigIsi, hFigCorr, hFigPos, hFigMap, hFigTime
     close_(get_fig_('FigTrial')); %close previous FigTrial figure
     close_(get_fig_('FigTrial_b')); %close previous FigTrial figure
-    S0 = button_CluWav_simulate_(1, [], S0); %select first clu
+    S0 = button_CluWav_simulate_(1, [], S0, 1); %select first clu TW
     auto_scale_proj_time_(S0);
     S0 = keyPressFcn_cell_(get_fig_cache_('FigWav'), {'z'}, S0); %zoom
     %S0.cS_log = load_(strrep(P.vcFile_prm, '.prm', '_log.mat'), 'cS_log', 0);
