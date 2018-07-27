cdd297cffcfb409143addaca826ebef043918efe
Alan Liddell <alan@vidriotech.com>
2018-07-27 12:20:01 -0400

    Tim's changes to randomSelect_

--- randomSelect_.m
+++ randomSelect_.m
@@ -2,6 +2,6 @@
 function viTime1 = randomSelect_(viTime1, nShow)
     if isempty(viTime1), return; end
     if numel(viTime1) > nShow
-        viTime1 = viTime1(randperm(numel(viTime1), nShow));
+        viTime1 = viTime1(ceil(numel(viTime1) .* rand(nShow, 1))); % TW
     end
 end %func
