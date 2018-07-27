4151e64ccddcb689b2a764a5f4e4cf45bf53bd20
Alan Liddell <alan@vidriotech.com>
2018-07-27 10:54:42 -0400

    Tim's changes to button_CluWav_

--- button_CluWav_.m
+++ button_CluWav_.m
@@ -18,8 +18,8 @@ function button_CluWav_(xyPos, vcButton)
         S0 = update_cursor_(S0, xPos, 1);
     end
     figure_wait_(1);
-    S0 = keyPressFcn_cell_(get_fig_cache_('FigWav'), {'j','t','c','i','v','e','f'}, S0); %'z'
-    auto_scale_proj_time_(S0);
+    S0 = keyPressFcn_cell_(get_fig_cache_('FigWav'), {'t','c','i','v','e','f'}, S0); % 'z','j'
+    % auto_scale_proj_time_(S0);
     set(0, 'UserData', S0);
     plot_raster_(S0);
     figure_wait_(0);
