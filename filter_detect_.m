fa1f257b96b22e6d66cdc9879b33bd4d00eec642
Alan Liddell <alan@vidriotech.com>
2018-07-27 11:26:47 -0400

    Tim's changes to filter_detect_

--- filter_detect_.m
+++ filter_detect_.m
@@ -15,53 +15,51 @@ function [mn1, nShift_post] = filter_detect_(mn, P, vcMode)
     nShift_post = 0;
     switch lower(vcMode)
         case 'ndist'
-        mn1 = ndist_filt_(mn, get_set_(P, 'ndist_filt', 5));
-        case 'chancor'
-        mn1 = chancor_(mn, P);
+            mn1 = ndist_filt_(mn, get_set_(P, 'ndist_filt', 5));
         case 'matched'
-        vrFilt_spk = get0_('vrFilt_spk');
-        if isempty(vrFilt_spk)
-            lim_ = round([3,5]/8 * size(mn,1));
-            mn_ = mn(lim_(1):lim_(2),:);
-            [vrFilt_spk, vrVaf, nShift_post] = calc_matched_filt_(mn_, P); %detect primary
-            set0_(vrFilt_spk, nShift_post);
-        else
-            nShift_post = get0_('nShift_post');
-        end
-        %         nShift_post = nShift_post_;
-        mn1 = int16(conv2(single(gather_(mn)), vrFilt_spk(:), 'same'));
-        %         mn1 = shift_mr_(mn1, nShift_post); % or do it later in the spike detection phase
-        %         figure; plot(xcorr(mn(:,41), mn1(:,41), 10));
-        %         nShift_post = P.spkLim(1)-1;
+            vrFilt_spk = get0_('vrFilt_spk');
+            if isempty(vrFilt_spk)
+                lim_ = round([3,5]/8 * size(mn,1));
+                mn_ = mn(lim_(1):lim_(2),:);
+                [vrFilt_spk, vrVaf, nShift_post] = calc_matched_filt_(mn_, P); %detect primary
+                set0_(vrFilt_spk, nShift_post);
+            else
+                nShift_post = get0_('nShift_post');
+            end
+            %         nShift_post = nShift_post_;
+            mn1 = int16(conv2(single(gather_(mn)), vrFilt_spk(:), 'same'));
+            %         mn1 = shift_mr_(mn1, nShift_post); % or do it later in the spike detection phase
+            %         figure; plot(xcorr(mn(:,41), mn1(:,41), 10));
+            %         nShift_post = P.spkLim(1)-1;
         case 'autocov'
-        mn1 = -int16(filt_corr(single(mn), 2));
+            mn1 = -int16(filt_corr(single(mn), 2));
         case 'std-chan'
-        mn1 = zeros(size(mn), 'like', mn);
-        for iSite=1:size(mn,2)
-            %mn_ = mn(:, P.miSites(viSites_use, iSite));
-            %vn_ref = mean(mn_(:,viSites_ref),2);
-            %mn_ = bsxfun(@minus, mn_(:,viSites_use), vn_ref);
-            % mn1(:, iSite) = -int16(std(single(mn(:, P.miSites(:, iSite))), 0, 2));
-            mn1(:, iSite) = -int16(std(single(mn(:, miSites(:,iSite))), 1, 2));
-            %mn1(:,iSite) = mean(mn_.^2,2) - mean(mn_,2).^2;
-            fprintf('.');
-        end
+            mn1 = zeros(size(mn), 'like', mn);
+            for iSite=1:size(mn,2)
+                %mn_ = mn(:, P.miSites(viSites_use, iSite));
+                %vn_ref = mean(mn_(:,viSites_ref),2);
+                %mn_ = bsxfun(@minus, mn_(:,viSites_use), vn_ref);
+                % mn1(:, iSite) = -int16(std(single(mn(:, P.miSites(:, iSite))), 0, 2));
+                mn1(:, iSite) = -int16(std(single(mn(:, miSites(:,iSite))), 1, 2));
+                %mn1(:,iSite) = mean(mn_.^2,2) - mean(mn_,2).^2;
+                fprintf('.');
+            end
 
         case 'std-time'
-        % envelop filter. this affects the threshold.
+            % envelop filter. this affects the threshold.
 
         case 'nmean'
-        mn1 = zeros(size(mn), 'like', mn);
-        for iSite=1:size(mn,2)
-            %mn_ = mn(:, P.miSites(viSites_use, iSite));
-            %vn_ref = mean(mn_(:,viSites_ref),2);
-            %mn_ = bsxfun(@minus, mn_(:,viSites_use), vn_ref);
-            % mn1(:, iSite) = -int16(std(single(mn(:, P.miSites(:, iSite))), 0, 2));
-            %mn1(:, iSite) = mn(:,iSite) - int16(mean(single(mn(:,miSites_ref(:,iSite))),2));
-            mn1(:, iSite) = int16(mean(mn(:,miSites(:,iSite)), 2));
-            %mn1(:,iSite) = mean(mn_.^2,2) - mean(mn_,2).^2;
-            fprintf('.');
-        end
+            mn1 = zeros(size(mn), 'like', mn);
+            for iSite=1:size(mn,2)
+                %mn_ = mn(:, P.miSites(viSites_use, iSite));
+                %vn_ref = mean(mn_(:,viSites_ref),2);
+                %mn_ = bsxfun(@minus, mn_(:,viSites_use), vn_ref);
+                % mn1(:, iSite) = -int16(std(single(mn(:, P.miSites(:, iSite))), 0, 2));
+                %mn1(:, iSite) = mn(:,iSite) - int16(mean(single(mn(:,miSites_ref(:,iSite))),2));
+                mn1(:, iSite) = int16(mean(mn(:,miSites(:,iSite)), 2));
+                %mn1(:,iSite) = mean(mn_.^2,2) - mean(mn_,2).^2;
+                fprintf('.');
+            end
 
     end %switch
     fprintf('\n\ttook %0.1fs\n', toc(t1));
