d3b1c6990c3e119bb41bdc89c20cc90374cfacbf
Alan Liddell <alan@vidriotech.com>
2018-08-18 12:17:24 -0400

    restore about_.m

--- meta/jrcVersion.m
+++ meta/jrcVersion.m
@@ -1,24 +1,29 @@
 %--------------------------------------------------------------------------
 % 9/29/17 JJJ: Displaying the version number of the program and what's used. #Tested
-function [vcVer, vcDate, vcVer_used] = jrcVersion(vcFile_prm)
-    if nargin<1, vcFile_prm = ''; end
-    vcVer = 'v3.2.5';
-    vcDate = '1/8/2018';
-    vcVer_used = '';
-    if nargout==0
-        fprintf('%s (%s) installed\n', vcVer, vcDate);
+function [versionStr, dateStr, versionUsed] = jrcVersion(paramFile)
+    if nargin < 1
+        paramFile = '';
     end
+
+    versionStr = 'v3.2.5';
+    dateStr = '1/8/2018';
+    versionUsed = '';
+
+    if nargout == 0
+        fprintf('%s (%s) installed\n', versionStr, dateStr);
+    end
+
     try
-        if isempty(vcFile_prm)
+        if isempty(paramFile)
             P = get0_('P');
             if ~isempty(P)
-                fprintf('\t%s used in %s\n', P.version, P.paramFile);
-                vcVer_used = P.version;
+                fprintf('\t%s used in %s\n', P.versionStr, P.paramFile);
+                versionUsed = P.versionStr;
             end
-        elseif fileExists(vcFile_prm)
-            P = loadParams(vcFile_prm);
-            fprintf('\t%s used in %s\n', P.version, vcFile_prm);
-            vcVer_used = P.version;
+        elseif fileExists(paramFile)
+            P = loadParams(paramFile);
+            fprintf('\t%s used in %s\n', P.versionStr, paramFile);
+            versionUsed = P.versionStr;
         end
     catch
         ;
