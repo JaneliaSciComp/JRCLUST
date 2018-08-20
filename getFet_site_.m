cb0012a79d9233f700f272dab93321982d33e884
Alan Liddell <alan@vidriotech.com>
2018-08-20 10:13:47 -0400

    whitespace and other readability

--- getFet_site_.m
+++ getFet_site_.m
@@ -5,20 +5,27 @@ function [vrFet1, vrTime1, vcYlabel, viSpk1] = getFet_site_(iSite, iClu, S0)
     % return feature correspojnding to a site and cluster
     % requiring subsampled info: cvrVpp_site and cmrFet_site. store in S0
 
-    if nargin < 2, iClu = []; end
-    if nargin<3, S0 = get(0, 'UserData'); end
-    % S_clu = S0.S_clu;
+    if nargin < 2
+        iClu = [];
+    end
+    if nargin < 3
+        S0 = get(0, 'UserData');
+    end
+
     P = S0.P;
-    if ~isfield(P, 'displayFeature'), P.displayFeature = 'vpp'; end
+    if ~isfield(P, 'displayFeature')
+        P.displayFeature = 'vpp';
+    end
+
     [vrFet1, viSpk1] = getFet_clu_(iClu, iSite, S0);
     vrTime1 = double(S0.spikeTimes(viSpk1)) / P.sampleRateHz;
 
     % label
     switch lower(P.displayFeature)
         case {'vpp', 'vmin'} %voltage feature
-        vcYlabel = sprintf('Site %d (\\mu%s)', iSite, P.displayFeature);
+            vcYlabel = sprintf('Site %d (\\mu%s)', iSite, P.displayFeature);
         otherwise %other feature options
-        vcYlabel = sprintf('Site %d (%s)', iSite, P.displayFeature);
+            vcYlabel = sprintf('Site %d (%s)', iSite, P.displayFeature);
     end
 
 end %func
--- manual/plotFigTime.m
+++ manual/plotFigTime.m
@@ -2,23 +2,28 @@
 function plotFigTime(S0)
     % plot FigTime window. Uses subsampled data
 
-    if nargin<1, S0 = get(0, 'UserData'); end
-    S_clu = S0.S_clu; P = S0.P;
+    if nargin < 1
+        S0 = get(0, 'UserData');
+    end
+
+    S_clu = S0.S_clu;
+    P = S0.P;
+
     [hFig, S_fig] = getCachedFig('FigTime');
 
     %----------------
     % collect info
     iSite = S_clu.clusterSites(S0.iCluCopy);
-    [vrFet0, vrTime0] = getFet_site_(iSite, [], S0); % plot background
-    [vrFet1, vrTime1, vcYlabel, viSpk1] = getFet_site_(iSite, S0.iCluCopy, S0); % plot iCluCopy
+    [backgroundFeatures, backgroundTimes] = getFet_site_(iSite, [], S0); % plot background
+    [primaryFeatures, primaryTimes, vcYlabel, viSpk1] = getFet_site_(iSite, S0.iCluCopy, S0); % plot iCluCopy
 
     vcTitle = '[H]elp; (Sft)[Left/Right]:Sites/Features; (Sft)[Up/Down]:Scale; [B]ackground; [S]plit; [R]eset view; [P]roject; [M]erge; (sft)[Z] pos; [E]xport selected; [C]hannel PCA';
     if ~isempty(S0.iCluPaste)
-        [vrFet2, vrTime2] = getFet_site_(iSite, S0.iCluPaste, S0);
+        [secondaryFeatures, secondaryTimes] = getFet_site_(iSite, S0.iCluPaste, S0);
         vcTitle = sprintf('Clu%d (black), Clu%d (red); %s', S0.iCluCopy, S0.iCluPaste, vcTitle);
     else
-        vrFet2 = [];
-        vrTime2 = [];
+        secondaryFeatures = [];
+        secondaryTimes = [];
         vcTitle = sprintf('Clu%d (black); %s', S0.iCluCopy, vcTitle);
     end
     time_lim = double([0, abs(S0.spikeTimes(end))] / P.sampleRateHz);
@@ -45,6 +50,7 @@ function plotFigTime(S0)
             setPositionConstraintFcn(S_fig.hRect, ...
             makeConstrainToRectFcn('imrect',time_lim, [-4000 4000]));
         end
+
         set(hFig, 'KeyPressFcn', @keyPressFigTime);
         S_fig.cvhHide_mouse = mouse_hide_(hFig, S_fig.hPlot0, S_fig);
         if ~isempty(P.time_tick_show) %tick mark
@@ -55,9 +61,9 @@ function plotFigTime(S0)
     % iFet = S_fig.iFet;
     % iFet = 1;
     if ~isfield(S_fig, 'iSite'), S_fig.iSite = []; end
-    update_plot_(S_fig.hPlot0, vrTime0, vrFet0);
-    update_plot_(S_fig.hPlot1, vrTime1, vrFet1);
-    update_plot_(S_fig.hPlot2, vrTime2, vrFet2);
+    update_plot_(S_fig.hPlot0, backgroundTimes, backgroundFeatures);
+    update_plot_(S_fig.hPlot1, primaryTimes, primaryFeatures);
+    update_plot_(S_fig.hPlot2, secondaryTimes, secondaryFeatures);
     imrect_set_(S_fig.hRect, time_lim, vpp_lim);
     mouse_figure(hFig, S_fig.hAx); % allow zoom using wheel
     % button click function to select individual spikes, all spikes plotted
@@ -76,18 +82,18 @@ function plotFigTime(S0)
 
     S_fig = mergeStructs(S_fig, makeStruct_(iSite, time_lim, P, vpp_lim, viSpk1));
     S_fig.csHelp = {...
-    'Up/Down: change channel', ...
-    'Left/Right: Change sites', ...
-    'Shift + Left/Right: Show different features', ...
-    'r: reset scale', ...
-    'a: auto-scale', ...
-    'c: show pca across sites', ...
-    'e: export cluster info', ...
-    'f: export cluster feature', ...
-    'Zoom: mouse wheel', ...
-    'H-Zoom: press x and wheel. space to reset', ...
-    'V-Zoom: press y and wheel. space to reset', ...
-    'Drag while pressing wheel: pan'};
+        'Up/Down: change channel', ...
+        'Left/Right: Change sites', ...
+        'Shift + Left/Right: Show different features', ...
+        'r: reset scale', ...
+        'a: auto-scale', ...
+        'c: show pca across sites', ...
+        'e: export cluster info', ...
+        'f: export cluster feature', ...
+        'Zoom: mouse wheel', ...
+        'H-Zoom: press x and wheel. space to reset', ...
+        'V-Zoom: press y and wheel. space to reset', ...
+        'Drag while pressing wheel: pan'};
 
     set(hFig, 'UserData', S_fig);
 end %func
