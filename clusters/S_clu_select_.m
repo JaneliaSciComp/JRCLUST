09dbf233d9b916bbb080d0d639b316b7e3d2e8ec
Alan Liddell <alan@vidriotech.com>
2018-08-21 13:30:42 -0400

    struct_select_ => subsetStructFields

--- clusters/S_clu_select_.m
+++ clusters/S_clu_select_.m
@@ -5,16 +5,19 @@ function S_clu = S_clu_select_(S_clu, viKeep_clu)
     % excl vnSpk_clu, clusterSites, clusterXPositions, clusterYPositions
 
     % Quality
-    csNames = fieldnames(S_clu);
-    if isempty(csNames), return; end
-    viMatch_v = cellfun(@(vi)~isempty(vi), cellfun(@(cs)regexp(cs, '^v\w*_clu$'), csNames, 'UniformOutput', false));
-    S_clu = struct_select_(S_clu, csNames(viMatch_v), viKeep_clu);
+    fieldNames = fieldnames(S_clu);
+    if isempty(fieldNames)
+        return;
+    end
 
-    viMatch_t = cellfun(@(vi)~isempty(vi), cellfun(@(cs)regexp(cs, '^t\w*_clu$'), csNames, 'UniformOutput', false));
-    S_clu = struct_select_(S_clu, csNames(viMatch_t), viKeep_clu, 3);
+    viMatch_v = cellfun(@(vi) ~isempty(vi), cellfun(@(cs) regexp(cs, '^v\w*_clu$'), fieldNames, 'UniformOutput', false));
+    S_clu = subsetStructFields(S_clu, fieldNames(viMatch_v), viKeep_clu);
 
-    viMatch_c = cellfun(@(vi)~isempty(vi), cellfun(@(cs)regexp(cs, '^c\w*_clu$'), csNames, 'UniformOutput', false));
-    S_clu = struct_select_(S_clu, csNames(viMatch_c), viKeep_clu);
+    viMatch_t = cellfun(@(vi)~isempty(vi), cellfun(@(cs)regexp(cs, '^t\w*_clu$'), fieldNames, 'UniformOutput', false));
+    S_clu = subsetStructFields(S_clu, fieldNames(viMatch_t), viKeep_clu, 3);
+
+    viMatch_c = cellfun(@(vi)~isempty(vi), cellfun(@(cs)regexp(cs, '^c\w*_clu$'), fieldNames, 'UniformOutput', false));
+    S_clu = subsetStructFields(S_clu, fieldNames(viMatch_c), viKeep_clu);
 
     % remap mrWavCor
     if isfield(S_clu, 'mrWavCor')
