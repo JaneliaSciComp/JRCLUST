e157e8b5d0534de00fddb6c4b16e0b7dc71ae336
Alan Liddell <alan@vidriotech.com>
2018-08-21 12:20:11 -0400

    fix "Undefined function or variable 'createFigurepsth_'."

---
+++ manual/createFigPSTH.m
@@ -0,0 +1,32 @@
+%--------------------------------------------------------------------------
+function [hFig, hFig_b] = createFigPSTH(hFig, hFig_b, P, nStims)
+
+    % Figure handle for the iCluCopy
+    [axoffset, axlen] = deal(.08, 1/nStims);
+
+    if ~tryIsValid(hFig)
+        hFig = createFigure('FigTrial', [.5  .5 .5 .5], P.trialFile, 0, 0);
+        [vhAx1, vhAx2] = deal(nan(nStims, 1));
+        for iStim = 1:nStims
+            axoffset_ = axoffset + (iStim-1) * axlen;
+            vhAx1(iStim) = axes('Parent', hFig, 'Position',[.08 axoffset_ .9 axlen*.68]);
+            vhAx2(iStim) = axes('Parent', hFig, 'Position',[.08 axoffset_ + axlen*.68 .9 axlen*.2]);
+        end
+        vcColor = 'k';
+        set(hFig, 'UserData', makeStruct_(vhAx1, vhAx2, vcColor));
+    end
+
+    % Figure handle for the iCluPaste
+    if ~tryIsValid(hFig_b)
+        hFig_b = createFigure('FigTrial_b', [.5  0 .5 .5], P.trialFile, 0, 0);
+        set(hFig_b, 'Visible', 'off');
+        [vhAx1, vhAx2] = deal(nan(nStims, 1));
+        for iStim = 1:nStims
+            axoffset_ = axoffset + (iStim-1) * axlen;
+            vhAx1(iStim) = axes('Parent', hFig_b, 'Position',[.08 axoffset_ .9 axlen*.68]);
+            vhAx2(iStim) = axes('Parent', hFig_b, 'Position',[.08 axoffset_ + axlen*.68 .9 axlen*.2]);
+        end
+        vcColor = 'r';
+        set(hFig_b, 'UserData', makeStruct_(vhAx1, vhAx2, vcColor));
+    end
+end %func
