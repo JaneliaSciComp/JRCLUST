6917b5947aea6094d6465ebf9cca81b644a16010
Alan Liddell <alan@vidriotech.com>
2018-08-28 14:05:52 -0400

    axes_new_ => newAxes

--- axes_new_.m
+++
@@ -1,8 +0,0 @@
-%--------------------------------------------------------------------------
-function hAx = axes_new_(hFig)
-    if ischar(hFig), hFig = figureByTag(hFig); end
-    figure(hFig); %set focus to figure %might be slow
-    clf(hFig);
-    hAx = axes();
-    hold(hAx, 'on');
-end %func
---
+++ manual/newAxes.m
@@ -0,0 +1,13 @@
+%--------------------------------------------------------------------------
+function hAx = newAxes(hFig)
+    % clear figure and instantiate a new Axes
+
+    if ischar(hFig)
+        hFig = figureByTag(hFig);
+    end
+
+    figure(hFig); % set focus to figure (might be slow)
+    clf(hFig);
+    hAx = axes(); % create default axes in current figure (hFig)
+    hold(hAx, 'on');
+end % func
