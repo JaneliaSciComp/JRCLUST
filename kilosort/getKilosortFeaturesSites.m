a4188a61a1ebabf161bbcddf373f491eb800f9e7
Alan Liddell <alan@vidriotech.com>
2018-08-28 15:25:17 -0400

    featuresMin, featuresMax => pc1, pc2

--- kilosort/getKilosortFeaturesSites.m
+++ kilosort/getKilosortFeaturesSites.m
@@ -1,13 +1,16 @@
 %--------------------------------------------------------------------------
-function [featuresMin, featuresMax] = getKilosortFeaturesSites(spikes, sitesOfInterest, S0)
+function [pc1, pc2] = getKilosortFeaturesSites(spikes, sitesOfInterest, S0)
+    % return 1st and 2nd (or 1st and 3rd, or 2nd and 3rd) principal components
+    % for each spike on sites of interest
+
     rez = S0.rez;
     S_clu = S0.S_clu;
-    
+
     nSites = numel(sitesOfInterest);
     nSpikes = numel(spikes);
-    
-    featuresMin = zeros(nSites, nSpikes);
-    featuresMax = zeros(nSites, nSpikes);
+
+    pc1 = zeros(nSites, nSpikes);
+    pc2 = zeros(nSites, nSpikes);
 
     pcFeatureIndices = rez.iNeighPC;
     spikeFeatures = permute(rez.cProjPC(spikes, :, :), [2 3 1]); % nPCs x nSites x nSpikes
@@ -22,11 +25,11 @@ function [featuresMin, featuresMax] = getKilosortFeaturesSites(spikes, sitesOfIn
         iTemplateSites = find(iSites(:, iTemplate));
         templateSites = pcFeatureIndices(iTemplateSites, template);
         siteIndices = ismember(sitesOfInterest, templateSites);
-        
+
         [~, sortIndices] = sort(templateSites);
-        featuresMin(siteIndices, spikeIndices) = spikeFeatures(S0.kspc(1), iTemplateSites(sortIndices), spikeIndices);
-        featuresMin(~siteIndices, spikeIndices) = nan;
-        featuresMax(siteIndices, spikeIndices) = spikeFeatures(S0.kspc(2), iTemplateSites(sortIndices), spikeIndices);
-        featuresMax(~siteIndices, spikeIndices) = nan;
+        pc1(siteIndices, spikeIndices) = spikeFeatures(S0.kspc(1), iTemplateSites(sortIndices), spikeIndices);
+        pc1(~siteIndices, spikeIndices) = nan; % PCs not occurring on this site get NaN
+        pc2(siteIndices, spikeIndices) = spikeFeatures(S0.kspc(2), iTemplateSites(sortIndices), spikeIndices);
+        pc2(~siteIndices, spikeIndices) = nan; % PCs not occurring on this site get NaN
     end
 end % func
