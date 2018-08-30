4406833b0cc5c4bae7095abd6ea4917c354a3d2b
Alan Liddell <alan@vidriotech.com>
2018-08-30 12:42:32 -0400

    some housekeeping in one-use functions

---
+++ clusters/mergePair.m
@@ -0,0 +1,31 @@
+%--------------------------------------------------------------------------
+function S_clu = mergePair(S_clu, P, cluster1, cluster2)
+    % merge a pair of clusters
+
+    % ensure cluster1 < cluster2
+    if cluster1 > cluster2
+        tmp = cluster2;
+        cluster2 = cluster1;
+        cluster1 = tmp;
+    end
+
+    % cluster1 takes all spikes; cluster2 gets nothing
+    S_clu.nSpikesPerCluster(cluster1) = sum(S_clu.nSpikesPerCluster([cluster1 cluster2]));
+    S_clu.nSpikesPerCluster(cluster2) = 0;
+
+    % update spike assignments
+    S_clu.spikeClusters(S_clu.spikeClusters == cluster2) = cluster1;
+    S_clu.spikesByCluster{cluster1} = find(S_clu.spikeClusters == cluster1);
+    S_clu.spikesByCluster{cluster2} = [];
+
+    try % TODO: this shouldn't need a try statement
+        S_clu.clusterNotes{cluster1} = '';
+        S_clu.clusterNotes{cluster2} = '';
+    catch
+    end
+
+    S_clu = S_clu_refrac_(S_clu, P, cluster1); % remove refrac
+    S_clu = S_clu_update_(S_clu, cluster1, P);
+    S_clu = delete_clu_(S_clu, cluster2);
+    S_clu = S_clu_remove_empty_(S_clu);
+end
--- manual/manualMerge.m
+++ manual/manualMerge.m
@@ -1,30 +1,40 @@
 %--------------------------------------------------------------------------
 function S0 = manualMerge(S0)
-    if nargin<1, S0 = []; end
-    if isempty(S0), S0 = get(0, 'UserData'); end
+    if nargin < 1 || isempty(S0)
+        S0 = get(0, 'UserData');
+    end
     P = S0.P;
 
-    if isempty(S0.secondarySelectedCluster)
-        msgbox_('Right-click a cluster to merge.', 1); return;
-    end
-    if S0.primarySelectedCluster == S0.secondarySelectedCluster
-        msgbox_('Cannot merge to itself.', 1); return;
+    cluster1 = S0.primarySelectedCluster;
+    cluster2 = S0.secondarySelectedCluster;
+
+    if isempty(cluster2) || cluster1 == cluster2
+        msgbox_('Nothing to merge.', 1);
+        return;
     end
 
-    figure_wait_(1); drawnow;
-    S0.S_clu = merge_clu_(S0.S_clu, S0.primarySelectedCluster, S0.secondarySelectedCluster, P);
+    figure_wait_(1);
+    drawnow;
+    fprintf('%s [W] merging Clu %d and %d\n', datestr(now, 'HH:MM:SS'), cluster1, cluster2);
+
+    % here's where the magic happens
+    S_clu = mergePair(S0.S_clu, P, cluster1, cluster2);
+    dialogAssert(clusterDataConsistent(S_clu), 'Cluster number is inconsistent after merging');
+    S0.S_clu = S_clu;
+
     set(0, 'UserData', S0);
-    plotFigWav(S0); %redraw plot
-    S0.primarySelectedCluster = min(S0.primarySelectedCluster, S0.secondarySelectedCluster);
+    plotFigWav(S0); % redraw plot
+
+    S0.primarySelectedCluster = min(cluster1, cluster2);
     S0.secondarySelectedCluster = [];
     set(0, 'UserData', S0);
+
     updatePlot(S0.hPaste, nan, nan);
     S0 = update_FigCor_(S0);
     S0 = button_CluWav_simulate_(S0.primarySelectedCluster, [], S0);
     S0 = save_log_(sprintf('merge %d %d', S0.primarySelectedCluster, S0.secondarySelectedCluster), S0);
     set(0, 'UserData', S0);
 
-    % msgbox_close(hMsg);
     figure_wait_(0);
     % S_clu = S0.S_clu;
 end % function
