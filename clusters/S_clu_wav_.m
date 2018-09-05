a4ab1dc44083f6579e819bddfb6256c1d88bd081
Alan Liddell <alan@vidriotech.com>
2018-09-05 10:19:34 -0400

    move clu_wav_ into S_clu_wav_

--- clusters/S_clu_wav_.m
+++ clusters/S_clu_wav_.m
@@ -11,7 +11,7 @@ function S_clu = S_clu_wav_(S_clu, clustersToUpdate, fSkipRaw)
     if nargin < 3
         fSkipRaw = 0;
     end
-    
+
     S0 = get(0, 'UserData');
     P = S0.P;
 
@@ -53,10 +53,10 @@ function S_clu = S_clu_wav_(S_clu, clustersToUpdate, fSkipRaw)
         % load waveform data from S_clu to alter
         tmrWav_spk_clu = S_clu.tmrWav_spk_clu;
         tmrWav_raw_clu = S_clu.tmrWav_raw_clu;
-        
+
         trWav_spk_clu = S_clu.trWav_spk_clu;
         trWav_raw_clu = S_clu.trWav_raw_clu;
-        
+
         tmrWav_raw_lo_clu = S_clu.tmrWav_raw_lo_clu;
         tmrWav_raw_hi_clu = S_clu.tmrWav_raw_hi_clu;
     else % update everything, don't bother loading data from S_clu
@@ -71,12 +71,12 @@ function S_clu = S_clu_wav_(S_clu, clustersToUpdate, fSkipRaw)
             if isempty(mrWav_clu1)
                 continue;
             end
-            
+
             [tmrWav_spk_clu(:, clusterSites1, iCluster), trWav_spk_clu(:, :, iCluster)] = ...
                 deal(bit2uV_(mrWav_clu1, P));
         end
         if fVerbose, fprintf('.'); end
-    end %clu
+    end % cluster
 
     % Compute spkraw
     if ~fSkipRaw
@@ -97,7 +97,7 @@ function S_clu = S_clu_wav_(S_clu, clustersToUpdate, fSkipRaw)
                 else
                     tmrWav_raw_lo_clu(:, clusterSites1, iCluster) = meanSubtract(mrWav_lo_clu1) * P.uV_per_bit;
                     tmrWav_raw_hi_clu(:, clusterSites1, iCluster) = meanSubtract(mrWav_hi_clu1) * P.uV_per_bit;
-                end                
+                end
             end
 
             if fVerbose, fprintf('.'); end
@@ -117,5 +117,55 @@ function S_clu = S_clu_wav_(S_clu, clustersToUpdate, fSkipRaw)
 
     if fVerbose
         fprintf('\n\ttook %0.1fs\n', toc(t1));
+    end
+end % function
+
+%--------------------------------------------------------------------------
+% 10/22/17 JJJ
+function [mrWav_clu1, clusterSites, mrWav_lo_clu1, mrWav_hi_clu1] = clu_wav_(S_clu, traces, cluster, S0)
+    if nargin < 4
+        S0 = get(0, 'UserData');
+    end
+
+    fUseCenterSpk = 0; % set to zero to use all spikes
+    nSamples_max = 1000;
+    fDrift_merge = getOr(S0.P, 'fDrift_merge', 0);
+
+    [mrWav_clu1, mrWav_lo_clu1, mrWav_hi_clu1] = deal([]);
+
+    centerSite = S_clu.clusterSites(cluster);
+    clusterSites = S0.P.miSites(:, centerSite); % neighborhood of cluster center site
+    clusterSpikes = S_clu.spikesByCluster{cluster};
+    clusterSpikeSites = S0.spikeSites(clusterSpikes); % center sites of spikes in cluster
+
+    if fUseCenterSpk % only take spikes occurring on the center site
+        isCentered = (clusterSpikeSites == centerSite);
+        clusterSpikes = clusterSpikes(isCentered);
+        clusterSpikeSites = clusterSpikeSites(isCentered);
+    end
+
+    if isempty(clusterSpikes)
+        return;
+    end
+
+    if ~fDrift_merge
+        viSpk_clu2 = spk_select_mid_(clusterSpikes, S0.spikeTimes, S0.P);
+        mrWav_clu1 = mean(single(traces(:,:,viSpk_clu2)), 3);
+        mrWav_clu1 = meanSubtract(mrWav_clu1); %122717 JJJ
+
+        return;
+    end
+
+    vrPosY_spk1 = S0.mrPos_spk(clusterSpikes,2); %position based quantile
+    vrYLim = quantile(vrPosY_spk1, [0,1,2,3]/3);
+    [viSpk_clu_, clusterSites_] = spk_select_pos_(clusterSpikes, vrPosY_spk1, vrYLim(2:3), nSamples_max, clusterSpikeSites);
+    mrWav_clu1 = nanmean_int16_(traces(:,:,viSpk_clu_), 3, fUseCenterSpk, centerSite, clusterSites_, S0.P); % * S0.P.uV_per_bit;
+
+    if nargout > 2
+        [viSpk_clu_, clusterSites_] = spk_select_pos_(clusterSpikes, vrPosY_spk1, vrYLim(1:2), nSamples_max, clusterSpikeSites);
+        mrWav_lo_clu1 = nanmean_int16_(traces(:,:,viSpk_clu_), 3, fUseCenterSpk, centerSite, clusterSites_, S0.P);
+
+        [viSpk_clu_, clusterSites_] = spk_select_pos_(clusterSpikes, vrPosY_spk1, vrYLim(3:4), nSamples_max, clusterSpikeSites);
+        mrWav_hi_clu1 = nanmean_int16_(traces(:,:,viSpk_clu_), 3, fUseCenterSpk, centerSite, clusterSites_, S0.P);
     end
 end % function
