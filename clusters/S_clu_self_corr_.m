1a7ad77415475968471b81ecbac121c7e662c397
Alan Liddell <alan@vidriotech.com>
2018-09-05 14:59:51 -0400

    move S_clu_self_corr__ into S_clu_self_corr_

--- clusters/S_clu_self_corr_.m
+++ clusters/S_clu_self_corr_.m
@@ -19,3 +19,26 @@ function selfcorr = S_clu_self_corr_(S_clu, iClu1, S0)
         selfcorr = S_clu_self_corr__(S_clu, spikeTraces, iClu1, spikeSites);
     end
 end % function
+
+%--------------------------------------------------------------------------
+function selfcorr = S_clu_self_corr__(S_clu, spikeWaveforms, iClu1, spikeSites)
+    % cluster self-correlation. low means bad. return 1-corr score
+    MAX_SAMPLE = 4000;
+    if nargin<4, spikeSites = get0_('spikeSites'); end
+
+    [viSpk_clu1, viiSpk_clu1] = getClusterCenteredSpikes(S_clu, iClu1, spikeSites);
+
+    viSpk_clu1 = randomSubsample(viSpk_clu1, MAX_SAMPLE);
+    % trWav1 = meanSubtract(single(spikeWaveforms(:,:,viSpk_clu1)));
+    trWav1 = spikeWaveforms(:,:,viSpk_clu1);
+    vrVpp = squeeze_(squeeze_(max(trWav1(:,1,:)) - min(trWav1(:,1,:))));
+    % vrVpp = sum(squeeze_(max(spikeWaveforms) - min(trWav1)));
+    [~, viSrt] = sort(vrVpp);
+    imid = round(numel(viSrt)/2);
+    mrWavA = meanSubtract(mean(trWav1(:, :, viSrt(1:imid)), 3));
+    mrWavB = meanSubtract(mean(trWav1(:, :, viSrt(imid+1:end)), 3));
+    % selfcorr = calc_corr_(mrWavA(:), mrWavB(:));
+    % selfcorr = mean(mean(zscore_(mrWavA) .* zscore_(mrWavB)));
+    % selfcorr = mean(zscore_(mrWavA(:)) .* zscore_(mrWavB(:)));
+    selfcorr = corr_(mrWavA(:), mrWavB(:));
+end % function
