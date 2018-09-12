b065d777e33c112054d7ab3a7efa959fa48e8bd2
Alan Liddell <alan@vidriotech.com>
2018-09-12 12:26:42 -0400

    HMT: discard a window of blank_period_ms around bad samples

--- car_reject_.m
+++ car_reject_.m
@@ -8,11 +8,20 @@ function [vlKeep_ref, vrMad_ref] = car_reject_(vrWav_mean1, P)
     % tbin_ref = .01; %10 msec bin
     vrWav_mean1 = single(vrWav_mean1);
     nwin = round(P.sRateHz * blank_period_ms / 1000);
-    if nwin <= 1
+    if nwin > 0 %nwin <= 1
         if nargout < 2
             vlKeep_ref = thresh_mad_(abs(vrWav_mean1), blank_thresh);
         else
             [vlKeep_ref, vrMad_ref] = thresh_mad_(abs(vrWav_mean1), blank_thresh);
+        end
+
+        if nwin > 1
+            over_thresh=find(~vlKeep_ref);
+            for crossing=over_thresh'
+                left = max(crossing-ceil(nwin/2), 1);
+                right = min(crossing+ceil(nwin/2), numel(vlKeep_ref));
+                vlKeep_ref(left:right)=0;
+            end
         end
     else
         vrRef_bin = std(reshape_vr2mr_(vrWav_mean1, nwin), 1,1);
