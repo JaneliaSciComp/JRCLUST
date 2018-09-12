6f536bb01dbd27e361fba33435dc433ccf4b841e
Alan Liddell <alan@vidriotech.com>
2018-09-12 14:28:20 -0400

    HMT: do not call restoredefaultpath

--- setpath_.m
+++ setpath_.m
@@ -4,10 +4,10 @@ function setpath_()
     persistent fPathSet
     if fPathSet, return; end
 
-    restoredefaultpath;
-    disp('Matlab path is temporarily reset to the factory default for this session.');
+    % restoredefaultpath;
+    % disp('Matlab path is temporarily reset to the factory default for this session.');
 
-    [vcPath_jrc, ~, ~] = fileparts(mfilename('fullpath')); % get current directory
-    addpath(vcPath_jrc);
+    % [vcPath_jrc, ~, ~] = fileparts(mfilename('fullpath')); % get current directory
+    % addpath(vcPath_jrc);
     fPathSet = 1;
 end
