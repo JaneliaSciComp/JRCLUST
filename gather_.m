021239a7b5dfe2fa64488135cbeafa3139a249b1
Alan Liddell <alan@vidriotech.com>
2018-09-12 10:09:33 -0400

    gather errors not obfuscated

--- gather_.m
+++ gather_.m
@@ -3,11 +3,7 @@ function varargout = gather_(varargin)
     for i=1:nargin
         varargout{i} = varargin{i};
         if isa(varargin{i}, 'gpuArray')
-            try
-                varargout{i} = gather(varargin{i});
-            catch
-                ;
-            end
+            varargout{i} = gather(varargin{i});
         end
     end
 end %func
