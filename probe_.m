1cf824684fd45690eda582c22658c1d70db67705
Alan Liddell <alan@vidriotech.com>
2018-09-13 12:20:13 -0400

    moves plot_probe_ into probe_ (local function only)

--- probe_.m
+++ probe_.m
@@ -15,25 +15,51 @@ function probe_(vcFile_prb)
         end
     end
     vcFile_prb = find_prb_(vcFile_prb);
-    % S_prb = file2struct_(vcFile_prb);
     S_prb = load_prb_(vcFile_prb);
-    % if ~isfield(S_prb, 'shank'), S_prb.shank = ones(size(S_prb.channels)); end
 
-    % hFig = figure; hold on;
     hFig = create_figure_('FigProbe', [0 0 .5 1], vcFile_prb);
-    % viShank = unique(S_prb.shank);
-    % vcColor_shank = 'kbgrcm'; % up to
-    % for iShank=1:numel(viShank)
-    %     viSite1 = find(S_prb.shank == iShank);
     hPatch = plot_probe_(S_prb.mrSiteXY, S_prb.vrSiteHW, S_prb.viSite2Chan, S_prb.viShank_site);
-    %     iShank1 = mod(iShank-1, numel(vcColor_shank))+1;
-    %     set(hPatch, 'EdgeColor', vcColor_shank(iShank1));
-    % end
-    % plot_probe_(mrSiteXY, vrSiteHW, viSite2Chan, vrVpp, hFig)
-    % vrPos0 = get(0, 'ScreenSize');
-    % set(hFig, 'OuterPosition', vrPos0, 'Color', 'w');
     axis equal;
-    % set(hFig, 'Name', vcFile_prb, 'NumberTitle', 'off');
     edit(vcFile_prb); %show probe file
     figure(hFig);
 end %func
+
+%% local functions
+function hPatch = plot_probe_(mrSiteXY, vrSiteHW, viSite2Chan, vrVpp, hFig)
+    if nargin<3, viSite2Chan=[]; end
+    if nargin<4, vrVpp=[]; end
+    if nargin<5, hFig=[]; end
+
+    if isempty(hFig)
+        hFig = gcf;
+    else
+        figure(hFig);
+    end
+
+    vrX = [0 0 1 1] * vrSiteHW(2);
+    vrY = [0 1 1 0] * vrSiteHW(1);
+
+    mrPatchX = bsxfun(@plus, mrSiteXY(:,1)', vrX(:));
+    mrPatchY = bsxfun(@plus, mrSiteXY(:,2)', vrY(:));
+    nSites = size(mrSiteXY,1);
+    if ~isempty(vrVpp)
+        hPatch = patch(mrPatchX, mrPatchY, repmat(vrVpp(:)', [4, 1]), 'EdgeColor', 'k', 'FaceColor', 'flat'); %[0 0 0], 'EdgeColor', 'none', 'FaceColor', 'flat', 'FaceVertexCData', [0 0 0], 'FaceAlpha', 0);
+        caxis([0, max(vrVpp)]);
+        colormap jet;
+    else
+        hPatch = patch(mrPatchX, mrPatchY, 'w', 'EdgeColor', 'k'); %[0 0 0], 'EdgeColor', 'none', 'FaceColor', 'flat', 'FaceVertexCData', [0 0 0], 'FaceAlpha', 0);
+    end
+    if ~isempty(viSite2Chan)
+        csText = arrayfun(@(i)sprintf('%d/%d', i, viSite2Chan(i)), 1:numel(viSite2Chan), 'UniformOutput', 0);
+    else
+        csText = arrayfun(@(i)sprintf('%d', i), 1:nSites, 'UniformOutput', 0);
+    end
+    hText = text(mrSiteXY(:,1), mrSiteXY(:,2), csText, ...
+    'VerticalAlignment', 'top', 'HorizontalAlignment', 'left');
+    axis_([min(mrPatchX(:)), max(mrPatchX(:)), min(mrPatchY(:)), max(mrPatchY(:))]);
+    title('Site# / Chan# (zoom: wheel; pan: hold wheel & drag)');
+    % vrPos = get(gcf, 'Position');
+    xlabel('X Position (\mum)');
+    ylabel('Y Position (\mum)');
+    mouse_figure(hFig);
+end
