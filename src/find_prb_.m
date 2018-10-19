0409c5e117dc890819a121aaf53655bc8c4f5b14
Alan Liddell <alan@vidriotech.com>
2018-10-19 16:36:08 -0400

    don't automatically append probe filename to probedir

--- src/find_prb_.m
+++ src/find_prb_.m
@@ -6,9 +6,7 @@ function vcFile_prb = find_prb_(vcFile_prb)
         error('Probe file not given');
     end
 
-    % Find a prb file
+    % check the probe directory
     probedir = fullfile(fileparts(jrcpath_()), 'prb');
-    vcFile_prb_ = fullfile(probedir, vcFile_prb);
-
-    vcFile_prb = search_file_(vcFile_prb_, probedir);
+    vcFile_prb = search_file_(vcFile_prb, probedir);
 end %func
