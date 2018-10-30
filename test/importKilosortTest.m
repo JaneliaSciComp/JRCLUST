ccf6d394edcf7e6c67588adbe44c2de6e450e535
Alan Liddell <alan@vidriotech.com>
2018-10-30 12:54:38 -0400

    actually test some things

--- test/importKilosortTest.m
+++ test/importKilosortTest.m
@@ -25,15 +25,33 @@ jrc('import-ksort', prmFile);
 end
 
 % TESTS
-function testFoo(testCase)
-one = 1;
-assertEqual(testCase, one, 1);
+function testTimesTemplatesSites(testCase)
+dataDir = getenv('JRCTESTDATA');
+testDir = fullfile(dataDir, 'importKilosort', 'eMouse');
+% load up test results
+S0 = load(fullfile(testDir, 'test_jrc.mat'));
+
+dataSources = fullfile(fileparts(dataDir), 'data_sources');
+rezFile = fullfile(dataSources, 'eMouse', 'rez.mat');
+
+% load up the rez file
+load(rezFile, 'rez');
+% lifted  from rezToPhy
+spikeTimes = uint64(rez.st3(:,1));
+spikeTemplates = uint32(rez.st3(:,2));
+
+assertTrue(testCase, all(spikeTimes == S0.viTime_spk));
+assertTrue(testCase, all(spikeTemplates == S0.S_clu.viTemplate_spk));
+assertTrue(testCase, all(spikeTemplates == S0.S_clu.viClu_auto));
 end
 
 % TEARDOWN
 function teardownOnce(testCase)
 dataDir = getenv('JRCTESTDATA');
 testDir = fullfile(dataDir, 'importKilosort', 'eMouse');
+prmFile = fullfile(testDir, 'test.prm');
+
+jrc('clear', prmFile);
 
 % clean up files
 delete(fullfile(testDir, 'test.prm'));
@@ -41,4 +59,5 @@ delete(fullfile(testDir, 'test_spkfet.jrc'));
 delete(fullfile(testDir, 'test_spkraw.jrc'));
 delete(fullfile(testDir, 'test_spkwav.jrc'));
 delete(fullfile(testDir, 'test-probe.mat'));
+delete(fullfile(testDir, 'test_jrc.mat'));
 end
