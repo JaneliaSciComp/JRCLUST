adaf6053a32db1176a8c24f17b12be19040d269e
Alan Liddell <alan@vidriotech.com>
2018-11-01 14:47:42 -0400

    allow switching features

--- src/manual/keyPressFcn_FigProj_.m
+++ src/manual/keyPressFcn_FigProj_.m
@@ -11,65 +11,74 @@ function keyPressFcn_FigProj_(hFig, event)
     figure_wait_(1);
     switch lower(event.Key)
         case {'uparrow', 'downarrow'}
-        rescale_FigProj_(event, hFig, S_fig, S0);
+            rescale_FigProj_(event, hFig, S_fig, S0);
 
         case {'leftarrow', 'rightarrow'} % change channels
-        fPlot = 0;
-        if strcmpi(event.Key, 'leftarrow')
-            if min(S_fig.viSites_show)>1
-                S_fig.viSites_show=S_fig.viSites_show-1;
-                fPlot = 1;
+            fPlot = 0;
+            if strcmpi(event.Key, 'leftarrow')
+                if min(S_fig.viSites_show)>1
+                    S_fig.viSites_show=S_fig.viSites_show-1;
+                    fPlot = 1;
+                end
+            else
+                if max(S_fig.viSites_show) < max(P.viSite2Chan)
+                    S_fig.viSites_show=S_fig.viSites_show+1;
+                    fPlot = 1;
+                end
             end
-        else
-            if max(S_fig.viSites_show) < max(P.viSite2Chan)
-                S_fig.viSites_show=S_fig.viSites_show+1;
-                fPlot = 1;
+            if fPlot
+                set(hFig, 'UserData', S_fig);
+                S0.P.viSites_show = S_fig.viSites_show;
+                plot_FigProj_(S0);
             end
-        end
-        if fPlot
-            set(hFig, 'UserData', S_fig);
-            S0.P.viSites_show = S_fig.viSites_show;
-            plot_FigProj_(S0);
-        end
 
         case 'r' %reset view
-        axis_([0 numel(viSites_show) 0 numel(viSites_show)]);
+            axis_([0 numel(viSites_show) 0 numel(viSites_show)]);
 
         case 's' %split
-        figure_wait_(0);
-        if ~isempty(S0.iCluPaste)
-            msgbox_('Select one cluster to split'); return;
-        end
-        S_plot1 = select_polygon_(S_fig.hPlot1);
-        if ~isempty(S_plot1)
-            [fSplit, vlIn] = plot_split_(S_plot1);
-            if fSplit
-                S_clu = split_clu_(S0.iCluCopy, vlIn);
-            else
-                update_plot2_proj_();
-                %                 delete_multi_(S_plot1.hPoly);
+            figure_wait_(0);
+            if ~isempty(S0.iCluPaste)
+                msgbox_('Select one cluster to split'); return;
             end
-        end
+            S_plot1 = select_polygon_(S_fig.hPlot1);
+            if ~isempty(S_plot1)
+                [fSplit, vlIn] = plot_split_(S_plot1);
+                if fSplit
+                    S_clu = split_clu_(S0.iCluCopy, vlIn);
+                else
+                    update_plot2_proj_();
+                    %                 delete_multi_(S_plot1.hPoly);
+                end
+            end
 
         case 'm'
-        ui_merge_(S0);
+            ui_merge_(S0);
 
         case 'f'
-        disp('keyPressFcn_FigProj_: ''f'': not implemented yet');
-        %         if strcmpi(P.vcFet_show, 'vpp')
-        %             S0.vcFet_show = P.vcFet;
-        %         else
-        %             S0.vcFet_show = 'vpp';
-        %         end
-        %         set(0, 'UserData', S0);
-        %         plot_FigProj_();
+            if get_set_(P, 'fImportKsort', 0) && strcmpi(P.vcFet_show, 'vpp')
+                P.vcFet_show = 'kilosort';
+            elseif strcmpi(P.vcFet_show, 'vpp')
+                P.vcFet_show = P.vcFet;
+            else
+                P.vcFet_show = 'vpp';
+            end
 
-        case 'b' %background spikes
-        toggleVisible_(S_fig.hPlot0);
+            set0_(P);
+            try
+                plot_FigProj_();
+            catch
+                P.vcFet_show = 'vpp';
+                set0_(P);
+                plot_FigProj_();
+            end
 
-        case 'h' %help
-        msgbox_(S_fig.csHelp, 1);
-    end %switch
+        case 'b' % background spikes
+            toggleVisible_(S_fig.hPlot0);
+
+        case 'h' % help
+            msgbox_(S_fig.csHelp, 1);
+    end % switch
+
     % drawnow;
     figure_wait_(0);
 end %func
