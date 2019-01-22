aac4c01feab0b90001766b172f6e393a14f10593
Alan Liddell <alan@vidriotech.com>
2019-01-22 12:44:14 -0500

    remove some unused files

--- +jrclust/+controllers/+curate/private/autoScaleProjTime.m
+++
@@ -1,5 +0,0 @@
-function autoScaleProjTime(hClust, hFigProj, hFigTime, selected)
-    %AUTOSCALEPROJTIME Automatically scale features in FigProj and FigTime
-    autoScaleFigProj(hFigProj, hClust, selected);
-    autoScaleFigTime(hFigTime, hClust, selected);
-end
