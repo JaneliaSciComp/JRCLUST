09651cc61f966c846c0ba4e8f384ac90a86836c9
Alan Liddell <alan@vidriotech.com>
2019-02-19 15:34:25 -0500

    fix misnamed package

--- @JRC/preview.m
+++ @JRC/preview.m
@@ -1,6 +1,6 @@
 function preview(obj)
     %PREVIEW Display Preview GUI
     obj.startParPool();
-    hPreview = jrclust.controllers.curate.PreviewController(obj.hCfg);
+    hPreview = jrclust.views.PreviewController(obj.hCfg);
     hPreview.preview();
 end
