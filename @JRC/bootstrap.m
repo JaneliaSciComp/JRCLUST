784a6de59d553771f5802b7e447df6c02877f521
Alan Liddell <alan@vidriotech.com>
2019-06-06 15:32:25 -0400

    Somewhat more intelligent .meta handling
    - New probe files for Neuropixels 3A and 3B (staggered and aligned)
    - Don't assume 3A if an Imec probe (ask!)
    - Check for prb file in working directory and try to use it first

--- @JRC/bootstrap.m
+++ @JRC/bootstrap.m
@@ -3,7 +3,7 @@ function bootstrap(obj, varargin)
     %   metafile: optional string; path (or glob) to meta file(s)
     if nargin > 1
         metafile_ = jrclust.utils.absPath(varargin{1});
-        if isempty(metafile_) % warn?
+        if isempty(metafile_) % TODO: warn?
             metafile = '';
             workingdir = pwd();
         elseif ischar(metafile_)
@@ -38,40 +38,7 @@ function bootstrap(obj, varargin)
 
     % first check for a .meta file
     if isempty(metafile)
-        dlgAns = questdlg('Do you have a .meta file?', 'Bootstrap', 'No');
-
-        switch dlgAns
-            case 'Yes' % select .meta file
-                [metafile, workingdir] = jrclust.utils.selectFile({'*.meta', 'SpikeGLX meta files (*.meta)'; '*.*', 'All Files (*.*)'}, 'Select one or more .meta files', workingdir, 1);
-                if all(cellfun(@isempty, metafile))
-                    return;
-                end
-
-                binfile = cellfun(@(f) jrclust.utils.subsExt(f, '.bin'), metafile, 'UniformOutput', 0);
-
-            case 'No' % select recording file
-                [binfile, workingdir] = jrclust.utils.selectFile({'*.bin;*.dat', 'SpikeGLX recordings (*.bin, *.dat)'; ...
-                                                                  '*.rhd', 'Intan recordings (*.rhd)'; ...
-                                                                  '*.*', 'All Files (*.*)'}, 'Select one or more raw recordings', workingdir, 1);
-                if all(cellfun(@isempty, binfile))
-                    return;
-                end
-
-                [~, ~, exts] = cellfun(@(f) fileparts(f), binfile, 'UniformOutput', 0);
-                uniqueExts = unique(exts);
-                if numel(uniqueExts) > 1
-                    error('Specify only a single file type');
-                end
-
-                ext = uniqueExts{:};
-                if strcmpi(ext, '.rhd')
-                    obj.bootstrapIntan(binfile);
-                    return;
-                end
-
-            case {'Cancel', ''}
-                return;
-        end
+        [metafile, binfile, workingdir] = getMetafile(workingdir);
     end
 
     % check for missing binary files
@@ -84,44 +51,9 @@ function bootstrap(obj, varargin)
     end
 
     % load metafile
-    if ~isempty(metafile)
-        SMeta_ = jrclust.utils.loadMetadata(metafile{1});
-        cfgData = struct('sampleRate', SMeta_.sampleRate, ...
-                         'nChans', SMeta_.nChans, ...
-                         'bitScaling', SMeta_.bitScaling, ...
-                         'headerOffset', 0, ...
-                         'dataType', SMeta_.dataType, ...
-                         'probe_file', fullfile(jrclust.utils.basedir(), 'probes', sprintf('%s.prb', SMeta_.probe)));
-
-        if isfield(SMeta_, 'sites')
-            cfgData.siteMap = SMeta_.sites;
-        end
-        if isfield(SMeta_, 'siteLoc')
-            cfgData.siteLoc = SMeta_.siteLoc;
-        end
-        if isfield(SMeta_, 'shankMap')
-            cfgData.shankMap = SMeta_.shankMap;
-        end
-
-        cfgData.rawRecordings = binfile;
-        cfgData.outputDir = workingdir;
-    else
-        cfgData.rawRecordings = binfile;
-        cfgData.outputDir = workingdir;
-    end
-
-    dlgAns = questdlg('Would you like to specify a probe file?', 'Bootstrap', 'No');
-    switch dlgAns
-        case 'Yes' % select .prb file
-            probedir = workingdir;
-            if isempty(dir(fullfile(workingdir, '*.prb')))
-                probedir = fullfile(jrclust.utils.basedir(), 'probes');
-            end
-            [probefile, probedir] = jrclust.utils.selectFile({'*.prb', 'Probe files (*.prb)'; '*.*', 'All Files (*.*)'}, 'Select a probe file', probedir, 0);
-            cfgData.probe_file = fullfile(probedir, probefile);
-
-        case {'Cancel', ''}
-            return;
+    cfgData = metaToConfig(metafile, binfile, workingdir);
+    if isempty(cfgData) % closed dialog, cancel bootstrap
+        return;
     end
 
     % construct the Config object from specified data
@@ -208,7 +140,7 @@ function bootstrap(obj, varargin)
         break;
     end
 
-    dlgAns = questdlg('Would you like to export advanced parameters as well?', 'Bootstrap', 'No');
+    dlgAns = questdlg('Would you like to export advanced parameters?', 'Bootstrap', 'No');
     switch dlgAns
         case 'Yes'
             hCfg_.save('', 1);
@@ -225,6 +157,137 @@ function bootstrap(obj, varargin)
 end
 
 %% LOCAL FUNCTIONS
+function [metafile, binfile, workingdir] = getMetafile(workingdir)
+dlgAns = questdlg('Do you have a .meta file?', 'Bootstrap', 'No');
+    switch dlgAns
+        case 'Yes' % select .meta file
+            [metafile, workingdir] = jrclust.utils.selectFile({'*.meta', 'SpikeGLX meta files (*.meta)'; '*.*', 'All Files (*.*)'}, 'Select one or more .meta files', workingdir, 1);
+            if all(cellfun(@isempty, metafile))
+                return;
+            end
+
+            binfile = cellfun(@(f) jrclust.utils.subsExt(f, '.bin'), metafile, 'UniformOutput', 0);
+
+        case 'No' % select recording file
+            [binfile, workingdir] = jrclust.utils.selectFile({'*.bin;*.dat', 'SpikeGLX recordings (*.bin, *.dat)'; ...
+                                                              '*.rhd', 'Intan recordings (*.rhd)'; ...
+                                                              '*.*', 'All Files (*.*)'}, 'Select one or more raw recordings', workingdir, 1);
+            if all(cellfun(@isempty, binfile))
+                return;
+            end
+
+            [~, ~, exts] = cellfun(@(f) fileparts(f), binfile, 'UniformOutput', 0);
+            uniqueExts = unique(exts);
+            if numel(uniqueExts) > 1
+                error('Specify only a single file type');
+            end
+
+            ext = uniqueExts{:};
+            if strcmpi(ext, '.rhd')
+                obj.bootstrapIntan(binfile);
+                return;
+            end
+
+        case {'Cancel', ''}
+            return;
+    end
+end
+
+function cfgData = metaToConfig(metafile, binfile, workingdir)
+    cfgData = struct('outputDir', workingdir);
+    cfgData.rawRecordings = binfile;
+
+    if isempty(metafile)
+        return;
+    end
+
+    SMeta = jrclust.utils.loadMetadata(metafile{1});
+    cfgData.sampleRate = SMeta.sampleRate;
+    cfgData.nChans = SMeta.nChans;
+    cfgData.bitScaling = SMeta.bitScaling;
+    cfgData.headerOffset = 0; % standard for SpikeGLX
+    cfgData.dataType = 'int16'; % standard for SpikeGLX
+
+    probeFile = getProbeFile(cfgData.outputDir, 0);
+
+    if isempty(probeFile) && SMeta.isImec % think we've got a Neuropixels probe
+        if ~isempty(SMeta.probeOpt) % 3A with option
+            probeFile = sprintf('imec3_opt%d.prb', SMeta.probeOpt);
+        else % 3A or 3B; ask
+            dlgAns = questdlg('It looks like you have a Neuropixels probe. Is this correct?', 'Bootstrap', ...
+                              'Yes', 'No', 'No'); % don't permit 'Cancel'
+            if isempty(dlgAns) % closed dialog; cancel
+                cfgData = [];
+                return;
+            end
+
+            if strcmp(dlgAns, 'Yes')
+                dlgAns = listdlg('PromptString', 'Specify your configuration', ...
+                                 'SelectionMode', 'single', ...
+                                 'ListString', {'Phase 3A', ...
+                                                'Phase 3B (Staggered)', ...
+                                                'Phase 3B (Aligned)', ...
+                                                'Custom configuration'});
+                switch dlgAns
+                    case 1
+                        probeFile = 'imec3a.prb';
+                    case 2
+                        probeFile = 'imec3b_staggered.prb';
+                    case 3
+                        probeFile = 'imec3b_aligned.prb';
+                    case 4
+                        probeFile = getProbeFile(cfgData.outputDir);
+                end
+            end
+        end
+    elseif isempty(probeFile)
+        probeFile = getProbeFile(cfgData.outputDir);
+    end
+
+    cfgData.probe_file = probeFile;
+end
+
+function probeFile = probeFileInDir(workingdir)
+    probeFile = '';
+
+    d = dir(fullfile(workingdir, '*.prb'));
+    if isempty(d)
+        return;
+    end
+
+    probeFile = fullfile(workingdir, d(1).name);
+end
+
+function probeFile = getProbeFile(workingdir, ask)
+    if nargin < 2
+        ask = 1;
+    end
+
+    probeFile = probeFileInDir(workingdir);
+
+    if ~isempty(probeFile) % found a probe file in working directory; confirm
+        dlgAns = questdlg(sprintf('Found probe file ''%s''. Use it?', probeFile));
+        if strcmp(dlgAns, 'Yes')
+            return;
+        else
+            probeFile = '';
+        end
+    end
+
+    if ask
+        dlgAns = questdlg('Would you like to specify a probe file?', 'Bootstrap', 'No');
+        if strcmp(dlgAns, 'Yes')
+            probedir = workingdir;
+            if isempty(dir(fullfile(workingdir, '*.prb')))
+                probedir = fullfile(jrclust.utils.basedir(), 'probes');
+            end
+
+            [probefile, probedir] = jrclust.utils.selectFile({'*.prb', 'Probe files (*.prb)'; '*.*', 'All Files (*.*)'}, ...
+                                                             'Select a probe file', probedir, 0);
+            probeFile = fullfile(probedir, probefile);
+        end
+    end
+end
 % function hCfg = bootstrapGUI() % WIP
 %     %BOOTSTRAPGUI Show all (common) parameters
 %     % load old2new param set and convert to new2old
