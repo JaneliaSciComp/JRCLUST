4280a025cf007e1eb7cf9b30ddb65156a58baf72
Alan Liddell <alan@vidriotech.com>
2019-07-16 11:43:07 -0400

    switch PCA algorithm to 'eig' in autoSplit

--- +jrclust/+curate/@CurateController/autoSplit.m
+++ +jrclust/+curate/@CurateController/autoSplit.m
@@ -181,7 +181,8 @@ function [assigns, pcaFeatures] = doAutoSplit(sampledSpikes, spikeFeatures, hFig
     %DOAUTOSPLIT
     % TODO: ask users number of clusters and split multi-way
     %Make automatic split of clusters using PCA + hierarchical clustering
-    [~, pcaFeatures, ~] = pca(double(sampledSpikes'), 'Centered', 1, 'NumComponents', 3);
+    [~, pcaFeatures, ~] = pca(double(sampledSpikes'), 'Centered', 1, ...
+                              'NumComponents', 3, 'Algorithm', 'eig');
     combinedFeatures = [spikeFeatures pcaFeatures];
     nSpikes = size(sampledSpikes, 2);
 
