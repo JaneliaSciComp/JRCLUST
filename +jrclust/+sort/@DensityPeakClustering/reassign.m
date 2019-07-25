92f1bdbf1249483ac93515e65bada4af2dee33e4
Alan Liddell <alan@vidriotech.com>
2019-07-25 16:56:15 -0400

    update reassign to use new commit mechanism

--- +jrclust/+sort/@DensityPeakClustering/reassign.m
+++ +jrclust/+sort/@DensityPeakClustering/reassign.m
@@ -1,26 +1,13 @@
-function reassign(obj, recompute)
+function success = reassign(obj)
     %REASSIGN Reassign clusters, e.g., after a change of parameters
+    success = 0;
     if obj.nEdits > obj.editPos % not at tip of edit history, back out
         warning('cannot branch from history; use revert() first');
         return;
     end
 
     obj.sRes = jrclust.sort.assignClusters(obj.dRes, obj.sRes, obj.hCfg);
-
-    % these fields are mutable so we need to store copies in obj
-    obj.spikeClusters = obj.initialClustering;
-    if isfield(obj.sRes, 'clusterCenters')
-        obj.clusterCenters = obj.sRes.clusterCenters;
-    else
-        obj.clusterCenters = [];
-    end
-    obj.clusterCentroids = [];
-
-    if recompute
-        obj.updateWaveforms();
-        obj.computeCentroids();
-        obj.computeQualityScores([]);
-    end
-
+    obj.commit(obj.spikeClusters, struct(), 'reassign clusters');
     obj.orderClusters('clusterSites');
+    success = 1;
 end
