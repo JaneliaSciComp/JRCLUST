a959cd3a4fd015aaf51cf8612a1bb36fc4bed27e
Adrian Bondy <adrian.bondy@gmail.com>
2020-03-10 21:16:06 -0400

    remove waveform-based similarity score in templateclustering-specific code
    because already implemented (better) in other parts of the code

--- +jrclust/+sort/@TemplateClustering/commit.m
+++ +jrclust/+sort/@TemplateClustering/commit.m
@@ -1,13 +1,48 @@
 function success = commit(obj, spikeClusters, metadata, msg)
     %COMMIT Commit a modification of clustering to history log
+    %% first run parent class commit method
     success = commit@jrclust.interfaces.Clustering(obj, spikeClusters, metadata, msg);
+    %% then update template similarity scores
     if success
-        if isempty(fieldnames(metadata)) % initial commit                     
-            obj = obj.calculateSimScore([]);          
+        if isempty(fieldnames(metadata)) % initial commit
+            obj.templatesByCluster = arrayfun(@(iC) unique(obj.spikeTemplates(obj.spikesByCluster{iC})), ...
+                                      1:obj.nClusters, 'UniformOutput', 0);
+
+            templateSim = zeros(obj.nClusters);
+            for iCluster = 1:obj.nClusters
+                iTemplates = obj.templatesByCluster{iCluster}; % unique template indices for spikes in this cluster
+
+                % compute cluster sim score, Phy style
+                sims = max(obj.sRes.simScore(iTemplates, :), [], 1);
+
+                for jCluster = iCluster:obj.nClusters
+                    jTemplates = obj.templatesByCluster{jCluster};
+                    templateSim(iCluster, jCluster) = max(sims(jTemplates));
+                    templateSim(jCluster, iCluster) = templateSim(iCluster, jCluster);
+                end
+            end
+
+            obj.templateSim = templateSim;
         elseif isfield(metadata, 'unitCount') && any(isnan(metadata.unitCount))
-            flagged = isnan(metadata.unitCount);        
-            obj = obj.calculateSimScore(flagged); 
+            flagged = find(isnan(metadata.unitCount));
+
+            obj.templatesByCluster(flagged) = arrayfun(@(iC) unique(obj.spikeTemplates(obj.spikesByCluster{iC})), ...
+                flagged, 'UniformOutput', 0);
+
+            % update cluster templates
+            for i = 1:numel(flagged)
+                iCluster = flagged(i);
+                iTemplates = obj.templatesByCluster{iCluster}; % unique template indices for spikes in this cluster
+
+                % compute cluster sim score, Phy style
+                sims = max(obj.sRes.simScore(iTemplates, :), [], 1);
+
+                for jCluster = 1:obj.nClusters
+                    jTemplates = obj.templatesByCluster{jCluster};
+                    obj.templateSim(iCluster, jCluster) = max(sims(jTemplates));
+                    obj.templateSim(jCluster, iCluster) = obj.templateSim(iCluster, jCluster);
+                end
+            end
         end
     end
 end
-
