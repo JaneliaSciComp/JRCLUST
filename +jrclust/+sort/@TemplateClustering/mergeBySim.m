7aed69fff0219d0528d5080d7c29e34f96090801
jenniferColonell <colonellj@janelia.hhmi.org>
2020-03-22 16:29:21 -0400

    changes to allow automerging of clusters by Waveform similarity calc in Template Clustering

--- +jrclust/+sort/@TemplateClustering/mergeBySim.m
+++ +jrclust/+sort/@TemplateClustering/mergeBySim.m
@@ -1,5 +1,42 @@
 function nMerged = mergeBySim(obj)
-    %MERGEBYSIM
-    nMerged = 0;
+
+    fprintf('Template clustering mergeBySim\n');
+    %MERGEBYSIM Automatically merge clusters by similarity score
+    obj.hCfg.updateLog('mergeBySim', 'Merging clusters by similarity', 1, 0);
+    waveformSim = obj.waveformSim;
+    nClustersPrev = size(waveformSim, 2);
+
+    % ensure targets of merges are smaller than units merging into them
+    waveformSim(tril(true(nClustersPrev))) = 0;
+    [maxScores, mergeTargets] = max(waveformSim);
+
+    % keep clusters whose maximum similarity to some other cluster
+    % is LESS than our threshold
+    underThresh = maxScores < obj.hCfg.maxUnitSim;
+
+    if all(underThresh)
+        nMerged = 0;
+        obj.hCfg.updateLog('mergeBySim', 'No clusters to merge', 0, 1);
+        return;
+    end
+
+    mergingUnits = find(~underThresh);
+
+    mergeTargets = mergeTargets(mergingUnits);
+    % intersection represents merge targets which are also set to merge
+    % into some other unit, and thus shouldn't be merged down (but can
+    % still be merged into)
+    intersection = ismember(mergingUnits, mergeTargets);
+    mergingUnits(intersection) = [];
+    mergeTargets(intersection) = [];
+
+    nMerged = numel(mergingUnits);
+
+    res = obj.mergeUnits(obj.spikeClusters, mergeTargets, mergingUnits);
+    msg = sprintf('merge %d units by similarity', nMerged);
+    obj.commit(res.spikeClusters, res.metadata, msg);
+    
+    obj.hCfg.updateLog('mergeBySim', sprintf('Finished merging clusters (was %d, now %d: %d merged; minimum score: %0.3f)', ...
+                                             nClustersPrev, obj.nClusters, nMerged, min(maxScores(~underThresh))), 0, 1);
 end
 
